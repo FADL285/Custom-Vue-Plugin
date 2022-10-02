8990ade3b42f4a798e003051bff78904d792e9c6
FADL285 <53804570+FADL285@users.noreply.github.com>
2022-10-02 16:06:32 +0200

    Allow Custom Default Options to plugin

--- src/toolTipPlugin/index.js
+++ src/toolTipPlugin/index.js
@@ -1,8 +1,14 @@
 import { defineAsyncComponent } from "vue";
+import { defu } from "defu";
 
 export const tooltipOptionsInject = Symbol();
 
 export function toolTipPlugin(app, options) {
+  options = defu(options, {
+    // Your Custom Default options.
+    arrow: false,
+  });
+
   app.provide(tooltipOptionsInject, options);
   app.component(
     "ToolTip",
